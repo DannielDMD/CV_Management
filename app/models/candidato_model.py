54916d0156bda9ac9a9cb00532964404af1caba4
DannielDMD <desmendezd@gmail.com>
2025-04-22 15:50:58 -0500

    Se modifica levemente el endpoint de eliminar candidato

--- app/models/candidato_model.py
+++ app/models/candidato_model.py
@@ -1,7 +1,17 @@
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Text, Date, TIMESTAMP
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    Boolean,
+    ForeignKey,
+    Text,
+    Date,
+    TIMESTAMP,
+)
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.core.database import Base
+
 
 class Candidato(Base):
     __tablename__ = "candidatos"
@@ -17,7 +27,9 @@ class Candidato(Base):
     id_cargo = Column(Integer, ForeignKey("cargos_ofrecidos.id_cargo"), nullable=False)
     trabaja_actualmente_joyco = Column(Boolean, nullable=False)
     ha_trabajado_joyco = Column(Boolean, nullable=False)
-    id_motivo_salida = Column(Integer, ForeignKey("motivos_salida.id_motivo_salida"), nullable=True)
+    id_motivo_salida = Column(
+        Integer, ForeignKey("motivos_salida.id_motivo_salida"), nullable=True
+    )
     tiene_referido = Column(Boolean, nullable=False)
     nombre_referido = Column(String(255), nullable=True)
     fecha_registro = Column(TIMESTAMP, server_default=func.current_timestamp())
@@ -31,7 +43,20 @@ class Candidato(Base):
     motivo_salida = relationship("MotivoSalida", back_populates="candidato")
 
     # Relaciones inversas con tablas dependientes
-    educaciones = relationship("Educacion", back_populates="candidato", cascade="all, delete-orphan")
-    experiencias = relationship("ExperienciaLaboral", back_populates="candidato", cascade="all, delete-orphan")
-    conocimientos = relationship("CandidatoConocimiento", back_populates="candidato")
-    preferencias = relationship("PreferenciaDisponibilidad", back_populates="candidato", cascade="all, delete-orphan")
+    educaciones = relationship(
+        "Educacion", back_populates="candidato", cascade="all, delete-orphan"
+    )
+    experiencias = relationship(
+        "ExperienciaLaboral", back_populates="candidato", cascade="all, delete-orphan"
+    )
+    conocimientos = relationship(
+        "CandidatoConocimiento",
+        back_populates="candidato",
+        cascade="all, delete-orphan",
+    )
+
+    preferencias = relationship(
+        "PreferenciaDisponibilidad",
+        back_populates="candidato",
+        cascade="all, delete-orphan",
+    )
